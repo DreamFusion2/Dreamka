61a2df51b497962d24128a52bcd255f33f20faff
Evai4ik <gryaznovF08@gmail.com>
2024-12-26 22:32:20 +0300

    Add files via upload

---
+++ dream/script.js
@@ -0,0 +1,4 @@
+// После 5 секунд добавляем класс для отображения кнопки
+setTimeout(function () {
+    document.getElementById('myButton').classList.add('show-button');
+}, 5000); // 5000 миллисекунд = 5 секунд
